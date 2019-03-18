212a787f75d092353b98a8cdbbd04a2f363b1f3d
Zhibin Liu <ljqx@live.cn>
2019-03-18 14:43:58 +0800

    fix lint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const util = require('util');
 const glob = util.promisify(require('glob'));
 const mkdirp = util.promisify(require('mkdirp'));
+
 const writeFile = util.promisify(fs.writeFile);
 const { getMetadata } = require('./lib/getMetadata');
 const { parseFullName } = require('./lib/parseFullName');
@@ -14,7 +15,7 @@ async function generateJSONSchema(endpoint, {
     if (ref === 'Edm/String') {
       return true;
     }
-    return !ref.startsWith('Edm')
+    return !ref.startsWith('Edm');
   },
   withEnumValue = false,
 } = {}) {
@@ -24,39 +25,43 @@ async function generateJSONSchema(endpoint, {
     withEnumValue,
   });
 
-  for (const namespace in schemas) {
-    if (!namespace.startsWith('http') && Object.getOwnPropertyNames(schemas[namespace]).length) {
-      const namespacePath = path.join(dist, parseFullName(namespace))
-      await mkdirp(namespacePath)
-      for (const name in schemas[namespace]) {
-        await writeFile(path.join(namespacePath, `${name}.json`), JSON.stringify(schemas[namespace][name], null, 2));
-      }
-    }
-  }
+  await Promise.all([Object.entries(schemas).filter(
+    ([namespace, namespaceSchemas]) => !namespace.startsWith('http')
+      && Object.getOwnPropertyNames(namespaceSchemas).length,
+  ).map(([namespace, namespaceSchemas]) => {
+    const namespacePath = path.join(dist, parseFullName(namespace));
+    return (async () => {
+      await mkdirp(namespacePath);
+      await Promise.all(Object.entries(namespaceSchemas).map(
+        ([name, schema]) => writeFile(path.join(namespacePath, `${name}.json`), JSON.stringify(schema, null, 2)),
+      ));
+    })();
+  })]);
 }
 
 function findNavigableSchemas(root, startingPoint) {
   function loadRawSchema(name) {
-    return require(`./${root}/${name}`)
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    return require(`./${root}/${name}`);
   }
 
-  const queue = [startingPoint]
-  const visited = new Set(queue)
-  const navigableSchemas = []
+  const queue = [startingPoint];
+  const visited = new Set(queue);
+  const navigableSchemas = [];
 
   while (queue.length) {
-    const first = queue.shift()
-    navigableSchemas.push(first)
+    const first = queue.shift();
+    navigableSchemas.push(first);
 
-    const schema = loadRawSchema(first)
-    for (let property of schema.$$ODataExtension.NavigationProperty || []) {
+    const schema = loadRawSchema(first);
+    for (const property of schema.$$ODataExtension.NavigationProperty || []) {
       const propertySchema = schema.properties[property];
-      const refType = propertySchema.type == 'array' ?
-        propertySchema.items.$ref :
-        propertySchema.$ref
+      const refType = propertySchema.type === 'array'
+        ? propertySchema.items.$ref
+        : propertySchema.$ref;
       if (typeof refType === 'string' && !visited.has(refType)) {
-        queue.push(refType)
-        visited.add(refType)
+        queue.push(refType);
+        visited.add(refType);
       }
     }
   }
@@ -68,16 +73,17 @@ async function bundle(root, {
   dist = 'schemas.json',
 } = {}) {
   const files = await glob('**/*.json', {
-    cwd: root
-  })
-  const bundled = {}
+    cwd: root,
+  });
+  const bundled = {};
   for (const file of files) {
     const name = file.substr(0, file.length - '.json'.length);
+    // eslint-disable-next-line global-require, import/no-dynamic-require
     bundled[name] = require(path.join(process.cwd(), root, file));
   }
-  await writeFile(dist, JSON.stringify(bundled))
+  await writeFile(dist, JSON.stringify(bundled));
 }
 
-module.exports.generateJSONSchema = generateJSONSchema
-module.exports.findNavigableSchemas = findNavigableSchemas
-module.exports.bundle = bundle
+module.exports.generateJSONSchema = generateJSONSchema;
+module.exports.findNavigableSchemas = findNavigableSchemas;
+module.exports.bundle = bundle;
